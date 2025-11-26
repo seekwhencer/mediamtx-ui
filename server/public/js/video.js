65f06bccdfb98ab20cefcf2de168a00be6ad4d25
seekwhencer <matthias.kallenbach@gmail.com>
2025-11-26 14:10:18 +0100

    retrying videoplayer

--- server/public/js/video.js
+++ server/public/js/video.js
@@ -18,8 +18,25 @@ export default class Video extends EventEmitter {
         this.element.className = 'cam';
         this.element.id = this.name;
 
+        this.loaded = false;
         this.streams.element.append(this.element);
+        this.element.onclick = e => this.play();
 
+        this.restartInterval = setInterval(() => {
+            const v = this.element;
+            console.log(this.label, this.name, 'RETRYING');
+
+            if (v.buffered.length !== 1 && v.played.length !== 1)
+                this.play();
+
+            if (v.buffered.length === 1 && v.played.length === 1)
+                clearInterval(this.restartInterval);
+
+        }, 1000);
+
+    }
+
+    play() {
         if (Hls.isSupported()) {
             const hls = new Hls();
             hls.loadSource(this.url);
@@ -27,5 +44,7 @@ export default class Video extends EventEmitter {
         } else {
             this.element.src = this.url;
         }
+
+        this.element.play();
     }
 }
