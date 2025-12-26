3d9ebc4a479bf2e2685aae597005e8cac4e8ba97
seekwhencer <matthias.kallenbach@gmail.com>
2025-12-26 14:08:50 +0100

    generate a config yaml from runtime config

--- server/lib/Routes/Config.js
+++ server/lib/Routes/Config.js
@@ -1,7 +1,4 @@
 import express from "express";
-import * as yaml from "js-yaml";
-import {parse, stringify} from 'yaml';
-
 
 export default class ConfigRoutes {
     constructor(routes) {
@@ -13,16 +10,10 @@ export default class ConfigRoutes {
         this.router = express.Router();
         this.router.use(express.json({limit: "10mb"}));
 
-        // Routes
+        // get config YAML
         this.router.get('/config', async (req, res) => {
             const yml = await this.config.getYaml();
             return res.status(200).send(yml);
-        });
-
-        // update server settings
-        this.router.patch('/config', (req, res) => {
-            console.log('>>> CONFIG PATCH REQUEST RECEIVED');
-            return res.status(200).json({message: "CONFIG PATCH"});
         });
     }
 }
